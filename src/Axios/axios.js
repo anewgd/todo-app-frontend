3741bc739474c0ea2891fe632b9406e63dfa2035
Kirubel-Fikru <kirubelfikruwsh@gmail.com>
2023-06-23 11:13:45 +0300

    Fix backend endpoint

--- src/Axios/axios.js
+++ src/Axios/axios.js
@@ -1,7 +1,7 @@
 import axios from "axios"
 const instance = axios.create({
   baseURL:
-    "http://mernbackend-env.eba-gqm2mhpr.us-east-1.elasticbeanstalk.com:8000/api",
+    "http://mernbackend-env.eba-gqm2mhpr.us-east-1.elasticbeanstalk.com/api",
 })
 export default instance
 //
