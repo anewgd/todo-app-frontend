2e9e6e1c1106ff16a006c95aa364c775dc015a60
anewgd <98820640+anewgd@users.noreply.github.com>
2023-06-23 12:50:05 +0300

    Update axios.js

--- src/Axios/axios.js
+++ src/Axios/axios.js
@@ -1,5 +1,5 @@
 import axios from "axios"
 const instance = axios.create({
-    baseURL:"http://localhost:8000/api"
+    baseURL:"http://todo-app-backend.eba-qqzuvyqz.us-east-1.elasticbeanstalk.com/api"
 })
 export default instance
